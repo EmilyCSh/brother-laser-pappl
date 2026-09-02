676aafc7948379387db7afbab5eea72df6195cca
Emily <info@emy.sh>
2026-09-02 20:56:03 +0200

    initial
    Signed-off-by: Emily <info@emy.sh>

---
+++ src/brother_laser.cpp
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ *  src/brother_laser.cpp
+ *
+ *  Copyright (C) Emily <info@emy.sh>
+ */
+
+export module brother_laser;
+export import :common;
+export import :printer;
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ *  src/main.cpp
+ *
+ *  Copyright (C) Emily <info@emy.sh>
+ */
+
+import brother_laser;
+
+auto main() -> int
+{
+    return 0;
+}
