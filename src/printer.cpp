40e8fc46bdaeb3007b6c1b171a2297ade95ad15b
Apollinaria Martiri <index@apoli.me>
2026-09-04 00:02:10 +0200

    Added printer energy states state is atomic and printer can be in three different states: working, idle, static. clang-tidy: disabled 'performance-enum-size'
    Signed-off-by: Apollinaria Martiri <index@apoli.me>

--- src/printer.cpp
+++ src/printer.cpp
@@ -6,6 +6,7 @@
  */
 
 module;
+#include <atomic>
 #include <print>
 #include <string>
 #include <cstddef>
@@ -19,8 +20,16 @@ import brother_laser.pappl;
 
 export namespace brother_laser {
 
+
 class Printer
 {
+    enum class EnergyState
+    {
+        WORKING,
+        IDLE,
+        SLEEP,
+    };
+
 private:
     struct PrivateConstructor
     {
@@ -31,9 +40,12 @@ private:
     std::string m_device_uri;
     std::shared_ptr<PapplDevice> m_device;
 
+    std::atomic<EnergyState> m_energy_state;
+
 public:
     Printer(PrivateConstructor /* private constructor */, std::string driver_name, std::string device_uri, std::shared_ptr<PapplDevice> device)
-        : m_driver_name(std::move(driver_name)), m_device_uri(std::move(device_uri)), m_device(std::move(device))
+        : m_driver_name(std::move(driver_name)), m_device_uri(std::move(device_uri)), m_device(std::move(device)),
+          m_energy_state(EnergyState::WORKING)
     {}
 
     ~Printer()
@@ -43,8 +55,8 @@ public:
 
     Printer(const Printer&)                    = delete;
     auto operator=(const Printer&) -> Printer& = delete;
-    Printer(Printer&&)                         = default;
-    auto operator=(Printer&&) -> Printer&      = default;
+    Printer(Printer&&)                         = delete;
+    auto operator=(Printer&&) -> Printer&      = delete;
 
     [[nodiscard]] static auto create_shared(const std::string& driver_name, const std::string& device_uri)
         -> std::expected<std::shared_ptr<Printer>, common::DeviceError>
@@ -69,6 +81,7 @@ public:
 
     [[nodiscard]] auto send(std::string_view command) noexcept -> std::expected<void, common::DeviceError>
     {
+        set_energystate(EnergyState::WORKING);
         return m_device->write_all(command);
     }
 
@@ -120,6 +133,20 @@ public:
     [[nodiscard]] auto testprint() noexcept -> std::expected<void, common::DeviceError>
     {
         return send_pjl_cmd("EXECUTE TESTPRINT");
+    }
+
+    [[nodiscard]] auto get_energystate() noexcept -> EnergyState
+    {
+        return m_energy_state.load(std::memory_order::acquire);
+    }
+
+    auto set_energystate(EnergyState state) -> void
+    {
+        m_energy_state.store(state, std::memory_order::release);
+    }
+
+    auto energystate_heartbeat() -> void {
+        
     }
 
 private:
