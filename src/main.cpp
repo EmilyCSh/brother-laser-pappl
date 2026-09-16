0f94d4fd26a258da8f6cade2efe34cff4405571e
Emily <info@emy.sh>
2026-09-16 14:05:28 +0200

    test alpine
    Signed-off-by: Emily <info@emy.sh>

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,40 @@ import brother_laser.common;
 import brother_laser.pappl;
 import brother_laser.printer;
 
-auto main() -> int
+//NOLINTBEGIN
+#include <pappl/pappl.h>
+static pappl_pr_driver_t drivers[] = {
+    {"test_pappl", "Test_PAPPL_printer", NULL, NULL},
+    {"brother_hl1110", "Brother HL-1110 series", NULL, NULL},
+};
+
+static bool driver_callback(
+    [[maybe_unused]] pappl_system_t* system,
+    [[maybe_unused]] const char* driver_name,
+    [[maybe_unused]] const char* device_uri,
+    [[maybe_unused]] const char* device_id,
+    [[maybe_unused]] pappl_pr_driver_data_t* driver_data,
+    [[maybe_unused]] ipp_t** driver_attrs,
+    [[maybe_unused]] void* data)
 {
-    return 0;
+    return true;
+}
+
+auto main(int argc, char* argv[]) -> int
+{
+    return (papplMainloop(
+        argc,
+        argv,
+        "0.1",
+        "Brother monochrome laser printer application",
+        (int)(sizeof(drivers) / sizeof(drivers[0])),
+        drivers,
+        NULL,
+        driver_callback,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL));
+    //NOLINTEND
 }
